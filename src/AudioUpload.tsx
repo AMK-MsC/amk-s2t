5c5e5f315817e8e7c160263a486ca84c0e54b2ee
Jens Thuestad <jens.andreas@live.no>
2023-01-19 16:12:05 +0100

    :sparkles: Countdown for loading whispermodel

--- src/AudioUpload.tsx
+++ src/AudioUpload.tsx
@@ -6,16 +6,33 @@ import { HuggingFaceAPI } from "./api";
 
 const AudioUpload = (): JSX.Element => {
 
-    const [countDown, setCountDown] = useState();
+    const [countDown, setCountDown] = useState<number>(20);
 
     useEffect(() => {
-        const response = HuggingFaceAPI.initializeModel();
-        console.log(response);
-    })
+        const initializeModel = async () => {
+            const response = await HuggingFaceAPI.initializeModel();
+            setCountDown(response.estimated_time)
+            console.log(countDown)
+        }
+
+        void initializeModel();
+    }, [])
+
+    //Count down timer based on countDown state and stop when countDown is 0
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            if (countDown > 0) {
+                setCountDown(countDown - 1);
+            }
+        }, 1000);
+        return () => clearTimeout(timer);
+    }, [countDown]);
+
+
     return (
         <Center>
             <Box>
-                <Text>Hei</Text>
+                <Text>Hei du har {countDown} sekund igjen å leva :p</Text>
             </Box>
         </Center>
     );
--- src/api.ts
+++ src/api.ts
@@ -5,18 +5,20 @@ const HuggingFaceAPI = {
         try {
             const response = await fetch("https://api-inference.huggingface.co/models/openai/whisper-large-v2",
                 {
-                    headers: { Authorization: `Bearer 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` },
+                    headers: { Authorization: `Bearer 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` },
                     method: "POST",
                     body: "start",
                 }
             )
+            console.log(`Response status code: ${response.status}`)
             if (response.status == 503){
                 const result = await response.json();
+                console.log(`Result: ${result.estimated_time}`);
                 return result;
             }
         }
         catch (error) {
-            console.log(error);
+            console.log(`Error: ${error}`);
         }
     }
 }
