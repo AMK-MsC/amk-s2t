76ac652ba09b4fe0b90cad19acd0aaaa9b4d7e8f
Øyvind Grutle <oyvindgru@gmail.com>
2023-01-23 13:46:02 +0100

    :sparkles: Hugging face inference API speech-to-text

--- src/AudioUpload.tsx
+++ src/AudioUpload.tsx
@@ -1,18 +1,25 @@
-import { Box, Center, Text } from "@chakra-ui/react";
+import { Box, Button, Center, Text, VStack } from "@chakra-ui/react";
 import { useEffect, useRef, useState } from "react";
-import { HuggingFaceAPI } from "./api";
+import HuggingFaceAPI from "./api";
 
 
 
 const AudioUpload = (): JSX.Element => {
 
-    const [countDown, setCountDown] = useState<number>(10);
+    const [countDown, setCountDown] = useState<number>(0);
+    const [audio, setAudio] = useState<Blob>();
+    const [loading, setLoading] = useState<boolean>(false);
+    const [transcript, setTranscript] = useState<string>();
 
     useEffect(() => {
         const initializeModel = async () => {
-            const response = await HuggingFaceAPI.initializeModel();
-            setCountDown(response.estimated_time)
-            console.log(countDown)
+            try {
+                const response = await HuggingFaceAPI.initializeModel();
+                setCountDown(parseInt(response.estimated_time))
+                console.log(countDown)
+            } catch (error) {
+                console.log(error);
+            }
         }
 
         void initializeModel();
@@ -36,24 +43,45 @@ const AudioUpload = (): JSX.Element => {
     const uploadAudio = async (event: React.ChangeEvent<HTMLInputElement>) => {
         const file = event.target.files?.[0];
         if (file) {
+            setAudio(file);
             const objectUrl = URL.createObjectURL(file);
+            console.log(objectUrl);
             if (audioRef.current) {
                 audioRef.current.src = objectUrl;
                 audioRef.current.play();
             }
         }
     };
-    
-    
+
+    const transcribeAudio = async (file: Blob) => {
+        setLoading(true);
+        if (!file) {
+            return;
+        }
+        const response = await HuggingFaceAPI.transcribeAudio(file);
+        console.log(response);
+        setLoading(false);
+        setTranscript(response.text)
+    };
+
+
 
     return (
         <Center>
             {countDown === 0 ? (
-                <Box>
-                    <Text>Model initialized. Please upload an audio file.</Text>
-                    <input type="file" onChange={uploadAudio} />
-                    <audio ref={audioRef} controls />
-                </Box>
+                <VStack>
+                    <Box>
+                        <Text>Model initialized. Please upload an audio file.</Text>
+                        <input type="file" onChange={uploadAudio} />
+                        <audio ref={audioRef} controls />
+                    </Box>
+                    <Box>
+                        {loading ? <Text>Transcribing audio. Please wait.</Text> : <Button onClick={() => transcribeAudio(audio!)}>Transcribe</Button>}
+                    </Box>
+                    <Box>
+                        <Text>{transcript}</Text>
+                    </Box>
+                </VStack>
             ) : (
                 <Box>
                     <Text>Initializing model. Please wait {countDown} seconds.</Text>
--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,3 @@
-
 const HuggingFaceAPI = {
     initializeModel: async () => {
         console.log("start model")
@@ -11,7 +10,7 @@ const HuggingFaceAPI = {
                 }
             )
             console.log(`Response status code: ${response.status}`)
-            if (response.status == 503){
+            if (response.status == 503) {
                 const result = await response.json();
                 console.log(`Result: ${result.estimated_time}`);
                 return result;
@@ -20,7 +19,30 @@ const HuggingFaceAPI = {
         catch (error) {
             console.log(`Error: ${error}`);
         }
+    },
+
+    transcribeAudio: async (audio: Blob) => {
+        console.log("audio inference")
+        try {
+            const response = await fetch("https://api-inference.huggingface.co/models/openai/whisper-large-v2",
+                {
+                    headers: { Authorization: `Bearer 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`, Language: "no" },
+                    method: "POST",
+                    body: audio,
+
+                }
+            )
+            console.log(`Response status code: ${response.status}`)
+            if (response.status == 200) {
+                const result = await response.json();
+                console.log(`Result: ${result.transcript}`);
+                return result;
+            }
+        } catch (error) {
+            console.log(`Error: ${error}`);
+            return error;
+        }
     }
 }
 
-export { HuggingFaceAPI };
+export default HuggingFaceAPI;
