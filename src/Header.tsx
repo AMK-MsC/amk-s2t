b6e3476990375a1b068723a74d38a35bfbea3581
Jens Thuestad <jens.andreas@live.no>
2023-01-23 10:34:57 +0100

    :pencil2: Header typo fixed

--- src/Header.tsx
+++ src/Header.tsx
@@ -6,7 +6,7 @@ const Header = (): JSX.Element => {
         <Box marginTop={0} w="100%">
             <Center borderColor="lightcyan" m="2rem auto">
                 <HStack>
-                    <Text fontSize="5xl" as="b" color="blue.100"> AMK TEXT-TO-SPEECH</Text>
+                    <Text fontSize="5xl" as="b" color="blue.100"> AMK SPEECH-TO-TEXT</Text>
                 </HStack>
             </Center>
         </Box>
