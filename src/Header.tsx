1c6e8490be398afc55c4606d9ac4d2261886a209
Øyvind Grutle <oyvindgru@gmail.com>
2023-02-03 11:42:27 +0100

    :lipstick: Update header

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,15 +1,17 @@
-import { Box, Center, Flex, HStack, Text } from "@chakra-ui/react";
+import { Box, Center, Flex, HStack, Spacer, Text } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
 
 const Header = (): JSX.Element => {
     return (
-        <Box marginTop={0} w="100%">
-            <Center borderColor="lightcyan" m="2rem auto">
-                <HStack>
+        <Center>
+            <Flex alignContent="center" padding={5}>
+                <Box>
                     <Text fontSize="5xl" as="b" color="blue.100">AMK SPEECH-TO-TEXT</Text>
-                </HStack>
-            </Center>
-        </Box>
+                </Box>
+            </Flex>
+            <ColorModeSwitcher />
+        </Center>
+
     );
 };
 
